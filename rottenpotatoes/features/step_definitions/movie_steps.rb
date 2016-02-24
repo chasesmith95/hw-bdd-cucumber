b2a2d2cfe468972da9df6470c54f86947994450f
chasesmith95 <chase.smth@gmail.com>
2016-02-24 02:42:53 +0000

    First part done

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -3,9 +3,9 @@
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
+    Movie.create(movie) 
     # you should arrange to add that movie to the database here.
   end
-  fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +14,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  first = page.body.index(e1) or return false
+  second = page.body.index(e2, first) or return false
+  return true
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +27,12 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  rating_list.split(", ").each do |rating|
+    uncheck("ratings_#{rating}") ? uncheck!=nil : check("ratings_#{rating}")
+  end
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  all('table#movies tr').count.should == Movie.all.length
 end
